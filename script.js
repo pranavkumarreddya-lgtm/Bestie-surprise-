b69402bdf9f72085064c04e996bc2df78aca2233
pranavkumarreddya-lgtm <pranavkumarreddya@gmail.com>
2026-02-24 19:29:47 +0530

    Add typing effect and slideshow features
    Implement typing effect and slideshow functionality.

---
+++ script.js
@@ -0,0 +1,37 @@
+let text = "Hacking Complete...";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("loading").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(showSlideshow, 2000);
+  }
+}
+
+function showSlideshow() {
+  document.getElementById("hacker").classList.add("hidden");
+  document.getElementById("slideshow").classList.remove("hidden");
+}
+
+let images = ["pic1.jpg", "pic2.jpg", "pic3.jpg"];
+let current = 0;
+
+function nextSlide() {
+  current++;
+  if (current < images.length) {
+    document.getElementById("slide").src = images[current];
+  } else {
+    document.getElementById("slideshow").classList.add("hidden");
+    document.getElementById("secret").classList.remove("hidden");
+  }
+}
+
+function showMessage() {
+  document.getElementById("secret").classList.add("hidden");
+  document.getElementById("final").classList.remove("hidden");
+}
+
+typeEffect();
